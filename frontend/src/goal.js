0e8224b2b3780b347553a071d41e07cb512f0cb5
Simon <seunglives@hotmail.com>
2020-08-24 09:27:15 -0700

    removed all methods from Goal

--- frontend/src/goal.js
+++ frontend/src/goal.js
@@ -6,15 +6,5 @@ class Goal {
         this.total_calories = total_calories
     }
 
-    renderGoal() {
-        let goalsResults = document.getElementById("goal")
-
-        goalsResults.innerHTML += 
-        `
-            <p id="total">${this.total_calories} calories</p>
-
-        `
-    }
-
    
 }
